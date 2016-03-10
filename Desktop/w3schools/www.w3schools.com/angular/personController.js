65530d0c77005f0ce2c0aef6d2e5f747d122e06d
devendrachhantyal <chhantyal.devan@gmail.com>
2016-03-10 10:20:09 +0545

    EmailApp

---
+++ Desktop/w3schools/www.w3schools.com/angular/personController.js
@@ -0,0 +1,11 @@
+function personController($scope) {
+    $scope.person = {
+        firstName: "John",
+        lastName: "Doe",
+        fullName: function() {
+            var x; 
+            x = $scope.person;
+            return x.firstName + " " + x.lastName;
+        }
+    };
+}
